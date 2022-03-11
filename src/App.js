df5c1dc9301abafddf017390aed87d80144d8e2f
牛梦林 <799978248@qq.com>
2022-03-12 00:40:33 +0800

    feat: 整合antd与less并搭建具有Ant Design风格的页面

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import  {Button} from 'antd';
+import './App.less';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        hello world！create-react-app
-        管理后台web工程
-      </header>
+        <Button type="primary">123456</Button>
+        <Button type="primary" loading>
+            Loading123
+        </Button>
     </div>
   );
 }
