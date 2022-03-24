5805a4413f8eba8b2200661c12de942f33e52bf6
牛梦林 <799978248@qq.com>
2022-03-24 21:18:39 +0800

    feat: 用户管理功能模块设计与实现

--- src/service/index.js
+++ src/service/index.js
@@ -1,8 +1,9 @@
 import {url} from './api';
-import {userList} from './user';
+import {userList,updateUser} from './user';
 
 export default {
-    userList
+    userList,
+    updateUser
 }
 const AUTH_TOKEN = 'MTU5Mjg1MDg3NDcwNw==';
 
@@ -15,11 +16,35 @@ export function get(api) {
         }
         )
 }
-
+export function put(api) {
+    /**
+     * 第一个参数为body参数，第二个参数为URL path或查询参数
+     */
+    return params=>{
+        const formData = new FormData();
+        Object.entries(params).forEach(([k,v])=>{
+            formData.append(k,v);
+        });
+        return queryParams=>fetch(buildParams(url+api,queryParams),
+            {
+                method:'PUT',
+                body:formData,
+                headers:{
+                    'auth-token':AUTH_TOKEN
+                }
+            });
+    }
+}
 function buildParams(url,params={}) {
     let newUrl = new URL(url);
-    Object.keys(params).forEach(key=>{
-        newUrl.searchParams.append(key,params[key]);
-    });
-    return newUrl.toString();
+    if (typeof params==='object'){
+        Object.keys(params).forEach(key=>{
+            newUrl.searchParams.append(key,params[key]);
+        });
+        return newUrl.toString();
+    }else{
+        //适配path参数
+        return url.endsWith("/")?url+params:url+"/"+params;
+    }
+
 }
--- src/service/user/index.js
+++ src/service/user/index.js
@@ -1,5 +1,8 @@
-import {get} from '../index';
+import {get,put} from '../index';
 import * as api from '../../service/api';
 export function userList(params){
     return get(api.api.userList)(params);
+}
+export function updateUser(params){
+    return put(api.api.updateUser)(params);
 }
