2cb7aff180f4b94737e728fc74f85383761990a3
牛梦林 <799978248@qq.com>
2022-03-27 00:21:03 +0800

    feat: 配置列表搜索与过滤功能实现

--- src/page/config/Index.js
+++ src/page/config/Index.js
@@ -1,39 +1,101 @@
 import React from 'react';
 import './Index.less';
-import {Table} from 'antd';
+import {Table,Input,Button} from 'antd';
+import {SearchOutlined} from '@ant-design/icons';
 import api from '../../service';
-const PAGE_SIZE=1;
+import Highlighter from 'react-highlight-words';
+const PAGE_SIZE=10;
 export default class Index extends React.Component{
     state={
       list:[],
       total:0,
-      loading:false
+      loading:false,
+      searchText:'',
+      searchedColumn:''
     };
+
+    getColumnSearchProps=dataIndex=>({
+        filterDropdown:({setSelectedKeys,selectedKeys,confirm,clearFilters})=>(
+            <div style={{padding:8}}>
+                <Input
+                    ref={node=>this.searchInput = node}
+                    placeholder={`搜索 ${dataIndex}`}
+                    value={selectedKeys[0]}
+                    onChange={e=>setSelectedKeys(e.target.value?[e.target.value]:[])}
+                    onPressEnter={()=>this.handleSearch(selectedKeys,confirm,dataIndex)}
+                    style={{width:188, marginBottom:8, display:'block'}}
+                />
+                <Button
+                    type='primary'
+                    onClick={()=>this.handleSearch(selectedKeys,confirm,dataIndex)}
+                    icon={<SearchOutlined/>}
+                    size='small'
+                    style={{width:90,marginRight:8}}
+                >
+                    搜索
+                </Button>
+                <Button
+                    size='small'
+                    style={{width:90}}
+                    onClick={()=>this.handleReset(clearFilters)}
+                >
+                    重置
+                </Button>
+            </div>
+        ),
+        filterIcon:filtered=>(
+            <SearchOutlined style={{color:filtered?'#1990ff':undefined}}/>
+        ),
+        onFilter:(value,record)=>
+            record[dataIndex]
+                .toString()
+                .toLowerCase()
+                .includes(value.toLowerCase())
+        ,onFilterDropdownVisibleChange: visible => {
+            if (visible){
+                setTimeout(()=>this.searchInput.select());
+            }
+        },
+        render:text=>(
+            //yarn add react-highlight-words 文案高亮
+            (this.state.searchedColumn===dataIndex)?
+                <Highlighter
+                    highlightStyle={{backgroundColor:'#ffc069',padding:0}}
+                    searchWords={[this.state.searchText]}
+                    autoEscape
+                    textToHighlight={text.toString()}
+                />:text && text.toString()
+        )
+    });
     columns = [
         {
             title:'id',
             dataIndex:'id',
             key:'id',
             width:'10%',
-            fixed:'left'
+            fixed:'left',
+            ...this.getColumnSearchProps('id')
         },
         {
             title:'namespace',
             dataIndex:'namespace',
             key:'namespace',
-            width:'20%'
+            width:'20%',
+            ...this.getColumnSearchProps('namespace')
         },
         {
             title:'version',
             dataIndex:'version',
             key:'version',
-            width:'20%'
+            width:'20%',
+            ...this.getColumnSearchProps('version')
         },
         {
             title:'发布时间',
             dataIndex:'createTime',
             key:'createTime',
-            width:'25%'
+            width:'25%',
+            ...this.getColumnSearchProps('createTime')
         },
         {
             title:<div className='config-operation'>操作</div>,
@@ -58,6 +120,20 @@ export default class Index extends React.Component{
     handleClick(item){
 
     }
+
+    handleSearch = (selectedKeys,confirm,dataIndex)=>{
+        confirm();
+        this.setState({
+            searchText: selectedKeys[0],
+            searchedColumn: dataIndex
+        })
+    };
+    handleReset = clearFilters => {
+        clearFilters();
+        this.setState({
+            searchText: ''
+        })
+    };
 
     loadData(pageIndex){
         // if (pageIndex <= this.pageIndex)return;
